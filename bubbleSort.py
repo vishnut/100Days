a7e93666dc792027ff98f34bd5eedfaeba9f220f
Vishnu Thiagarajan <thiagarajanvishnu@gmail.com>
2017-02-27 23:50:08 -0600

    bubble

---
+++ bubbleSort.py
@@ -0,0 +1,13 @@
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+numSwaps = 0
+
+for i in range(len(a)):
+    for j in range(len(a) - 1):
+        if a[j] > a[j+1]:
+            a[j],a[j+1] = a[j+1],a[j]
+            numSwaps += 1
+
+print("Array is sorted in {} swaps.".format(numSwaps))
+print("First Element: {}".format(a[0]))
+print("Last Element: {}".format(a[len(a) - 1]))
