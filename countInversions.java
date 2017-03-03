90abcd1a61db04db1693a0efb5641fd54661b874
Vishnu Thiagarajan <thiagarajanvishnu@gmail.com>
2017-03-02 23:18:41 -0600

    inversions

---
+++ countInversions.java
@@ -0,0 +1,79 @@
+/*
+Written in Java because the Python solution
+is not fast enough for HackerRank. 
+Merge Sort taken from 
+http://www.java2novice.com/java-sorting-algorithms/merge-sort/
+Adapted to count the number of inversions
+*/
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static long v = 0;
+    public static int[] array;
+    public static int[] tempMergArr;
+    public static int length;
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int arr[] = new int[n];
+            for(int arr_i=0; arr_i < n; arr_i++){
+                arr[arr_i] = in.nextInt();
+            }
+            v = 0;
+            sort(arr);
+            System.out.println(v);
+        }
+    }
+    
+    public static void sort(int inputArr[]) {
+        array = inputArr;
+        length = inputArr.length;
+        tempMergArr = new int[length];
+        doMergeSort(0, length - 1);
+    }
+ 
+    private static void doMergeSort(int lowerIndex, int higherIndex) {
+         
+        if (lowerIndex < higherIndex) {
+            int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
+            doMergeSort(lowerIndex, middle);
+            doMergeSort(middle + 1, higherIndex);
+            mergeParts(lowerIndex, middle, higherIndex);
+        }
+    }
+ 
+    private static void mergeParts(int lowerIndex, int middle, int higherIndex) {
+        for (int i = lowerIndex; i <= higherIndex; i++) {
+            tempMergArr[i] = array[i];
+        }
+        int i = lowerIndex;
+        int j = middle + 1;
+        int k = lowerIndex;
+        while (i <= middle && j <= higherIndex) {
+            if (tempMergArr[i] <= tempMergArr[j]) {
+                array[k] = tempMergArr[i];
+                i++;
+            } else {
+                array[k] = tempMergArr[j];
+                j++;
+                v += (1 + middle - i);
+            }
+            k++;
+        }
+        while (i <= middle) {
+            array[k] = tempMergArr[i];
+            k++;
+            i++;
+        }
+ 
+    }
+}
